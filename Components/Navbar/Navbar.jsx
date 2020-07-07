f24eb480e6040c72f23aea8d468c31afa520c8b6
Dipak Roy <59052983+dipak1800@users.noreply.github.com>
2020-07-07 21:31:56 +0530

    Did CRA fot the application

---
+++ Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Style from "./Navbar.module.scss";
+class Navbar extends Component {
+  render() {
+    return <div>navbar</div>;
+  }
+}
+
+export default Navbar;
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "../Components/Navbar/Navbar";
+
+function App() {
+  return <>hello world</>;
+}
+
+export default App;
